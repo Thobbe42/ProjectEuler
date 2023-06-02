9f3c87f98167e72ddf2c6fca0367e91b8058b4c1
Thorben <thorbenlinker@gmail.com>
2023-06-02 09:25:06 +0200

    Updated LargestPrimeFactor to correctly calculate if a number is prime

--- LargestPrimeFactor.s
+++ LargestPrimeFactor.s
@@ -1,21 +1,23 @@
 .data
 
 format_string: .asciz "Result: %d\n"
-val: .word 5
+val: .word 117
 
 .global main
 .extern printf
 
 main:
 	ldr r1, val /* load predefined value into r1 */
+
+	/* call is_prime with r1 as parameter, selut stored in r0 */
 	push {lr}
 	push {r1}
 	bl is_prime
 	pop {lr}
 
-	mov r1, r0
-	ldr r0, val
-	b printf
+	mov r1, r0 /* move result to r1 */
+	ldr r0, =format_string /* load format string into r0 */
+	b printf 
 
 	bx lr
 
@@ -27,67 +29,71 @@ is_prime:
 
 	/* check edge cases */
 	cmp r4, #3
-	bge skip
-	mov r0, #1
+	bgt skip
+	mov r0, #1 /* return 1 if value if smaller than 3 */
 	b end
 
 	skip:
 	push {lr}
 	mov r6, #2
+	/* push value and 2 on the stack as parameters for mod */
 	push {r6}
 	push {r4}
 	bl mod /* call mod with r4 and 2 */
 	pop {r5} /* r5 = r4 % 2 */
 	pop {lr}
 
-	cmp r5, #0
+	cmp r5, #0 /* r5 = r4 % 2 = 0 ? */
 	bgt skip_2
-	mov r0, #0
+	mov r0, #0 /* return 0 */
 	b end
 
 	skip_2:
 	push {lr}
 	mov r6, #3
+	/* push value and 3 on the stack as parameters for mod */
 	push {r6}
 	push {r4}
 	bl mod /* call mod with r4 and 3 */
 	pop {r5} /* r5 = r4 % 3 */
 	pop {lr}
 
-	cmp r5, #3
+	cmp r5, #0 /* r5 = r4 % 3 = 0 ? */
 	bgt skip_3
-	mov r0, #0
+	mov r0, #0 /* return 0 */
 	b end
 
 	skip_3:
-	mov r5, #5 /* mmove loop index to r5 */
+	mov r5, #5 /* move loop index to r5 */
 
 	loop:
-	mul r6, r5, r5
-	cmp r6, r4
+	mul r6, r5, r5 /* r6 = i + i */
+	cmp r6, r4 /* compare r6 to val */
 	mov r0, #1
-	bgt end /* end and return true id i * i > val */
+	bgt end /* end and return true if i * i > val */
 
 	push {lr}
+	/* push val and currentindex i onto the stack as parameters for mod */
 	push {r5}
 	push {r4}
 	bl mod /* call mod with val and index */
-	pop {r7}
+	pop {r7} /* r7 = val % i */
 	pop {lr}
 
-	cmp r7, #0
-	mov r0, #0
+	cmp r7, #0 /* r7 = val % i = 0 ? */
+	mov r0, #0 /* return 0 */
 	bgt end
 
-	add r7, r5, #1
+	add r7, r5, #2 /* r7 = index +2 */
 	push {lr}
+	/* push val and i+2 onto the stack as parameters for mod */
 	push {r7}
 	push {r4}
 	bl mod /* call mod with val and index +2 */
-	pop {r7}
+	pop {r7} /* r7 = val % i+2 */
 	pop {lr}
 
-	cmp r7, #0
+	cmp r7, #0 /* r7 = val % i+2 = 0 ? */
 	bgt end
 
 	add r5, r5, #6 /* add 6 to the loop index */
@@ -103,7 +109,7 @@ mod:
 	pop {r8} /* r8 = div */
 
 	udiv r9, r7, r8 /* r9 = val / div */
-	mul r8, r9, r7 /* r8 = r9 * val */
+	mul r8, r9, r8 /* r8 = r9 * div */
 	sub r7, r7, r8 /* r7 = val % div */
 
 	push {r7} /* push result on the stack */
